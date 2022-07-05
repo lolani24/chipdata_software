a50dba8072eb82207ef77a22b805cb0b5439986d
alliyuh <108543588+alliyuh@users.noreply.github.com>
2022-07-05 09:27:53 -0700

    login UI and python authorization
    changed login UI and python authorization

--- login/auth.py
+++ login/auth.py
@@ -1,15 +1,30 @@
-from flask import Blueprint, render_template
+from pickle import FALSE, TRUE
+from pip import main
+from flask import Blueprint, render_template, request 
 
 auth_blueprint = Blueprint('auth', __name__, template_folder='templates')
 
-@auth_blueprint.route('/login')
+if __name__=='__main__':
+    auth_blueprint.run()
+
+@auth_blueprint.route('/login', methods=["GET", "POST"])
 def login():
     return render_template("login.html")
-
-
-#trial 
-
+def authorize():
+    if request.method == "POST": 
+        p_username = request.form.get("fusername")  
+        p_password = request.form.get("fpassword") 
+        if p_username == "aranlab" and p_password == "andres_23":
+            return render_template("home.html")
+        else:
+            return "Incorrect Login Information"
+    else:
+        return "Invalid Method"
 
 @auth_blueprint.route('/logout')
 def logout():
     return "logout page"
+
+
+#user aranlab
+#password andres_23
