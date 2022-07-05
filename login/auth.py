327580cd0614b61230e78fd96482ca8f9112c1c2
alliyuh <108543588+alliyuh@users.noreply.github.com>
2022-07-05 10:18:39 -0700

    working login page
    login page now takes you to home page
    -incorrect login works too

--- login/auth.py
+++ login/auth.py
@@ -1,30 +1,34 @@
 from pickle import FALSE, TRUE
 from pip import main
-from flask import Blueprint, render_template, request 
-
+from flask import Blueprint, render_template, request
+ 
 auth_blueprint = Blueprint('auth', __name__, template_folder='templates')
-
+ 
 if __name__=='__main__':
     auth_blueprint.run()
-
-@auth_blueprint.route('/login', methods=["GET", "POST"])
+ 
+@auth_blueprint.route('/login')
 def login():
     return render_template("login.html")
+   
+@auth_blueprint.route('/login', methods =["GET", "POST"])
 def authorize():
-    if request.method == "POST": 
+    if request.method == "POST":
         p_username = request.form.get("fusername")  
-        p_password = request.form.get("fpassword") 
+        p_password = request.form.get("fpassword")
         if p_username == "aranlab" and p_password == "andres_23":
             return render_template("home.html")
         else:
             return "Incorrect Login Information"
     else:
         return "Invalid Method"
-
+ 
 @auth_blueprint.route('/logout')
 def logout():
     return "logout page"
-
-
+ 
+ 
 #user aranlab
 #password andres_23
+ 
+
