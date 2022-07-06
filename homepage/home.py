f82cdd09bcfa8f5b7c9ed76ea7c5a207508b89f3
alliyuh <108543588+alliyuh@users.noreply.github.com>
2022-07-06 16:30:45 -0700

    workish login nav bar

--- homepage/home.py
+++ homepage/home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, url_for 
-
+#from login.auth import authorize, correct_login
+#if correct_login == True, go to home, elif correct_login == False go to login
 home_blueprint = Blueprint('home', __name__, template_folder='templates')
 
 @home_blueprint.route('/')
--- login/auth.py
+++ login/auth.py
@@ -1,11 +1,10 @@
 from pickle import FALSE, TRUE
 from pip import main
 from flask import Blueprint, render_template, request
- 
+
 auth_blueprint = Blueprint('auth', __name__, template_folder='templates')
- 
-if __name__=='__main__':
-    auth_blueprint.run()
+
+correct_login = None
  
 @auth_blueprint.route('/login')
 def login():
@@ -17,11 +16,12 @@ def authorize():
         p_username = request.form.get("fusername")  
         p_password = request.form.get("fpassword")
         if p_username == "aranlab" and p_password == "andres_23":
-            return render_template("home.html")
+            
+            return render_template("home.html", correct_login = True)
         else:
             return "Incorrect Login Information"
     else:
-        return "Invalid Method"
+        return "Invalid Method" 
  
 @auth_blueprint.route('/logout')
 def logout():
